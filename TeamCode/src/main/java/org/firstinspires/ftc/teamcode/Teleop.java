68651108390938a95a1bfe3fb57a188602c1d82f
SESI Mega Snakes FTC <megasnakesftc@gmail.com>
2026-07-04 16:32:35 -0300

    Atualizacao do item anterior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Teleop,,, group = "TeleOp")
+@TeleOp(name = "Teleoperado", group = "TeleOp")
 public class Teleop extends LinearOpMode {
 
     private DcMotor leftBack;
