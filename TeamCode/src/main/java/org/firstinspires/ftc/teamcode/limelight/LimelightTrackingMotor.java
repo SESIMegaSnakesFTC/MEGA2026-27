f4f9de3461d2672ad84bbb52ff5ff4df3a3b99dd
rogenski <joaopedrorogenski@gmail.com>
2026-08-27 10:29:18 -0300

    Códigos atualizados em fase de teste

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/LimelightTrackingMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/LimelightTrackingMotor.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.limelight;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
